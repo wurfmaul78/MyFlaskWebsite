db0385de1300cbcc0c151645c0f8eae92965787c
wurfmaul78 <gerhard.posch@outlook.com>
2021-12-26 20:05:56 +0100

    Individuelle Begrüssung und erste HTML Formata

--- start.py
+++ start.py
@@ -5,7 +5,14 @@ app = Flask (__name__)
 
 @app.route("/")
 def home():
-    return "Hello world"
+    return "<h1>Welcome on Gerhard Posch&#39;s Website</h1><p>This is where</p>"
+
+@app.route("/name/<ags>")
+def agskey(ags):
+    if ags == "Gerhard":
+        return f"Guten Tag Eure Koenigliche Hoheit, {escape(ags)}"
+    else:
+        return f"Guten Tag, {escape(ags)}"
 
 if "__name__"  == "__main__":
     app.run()
