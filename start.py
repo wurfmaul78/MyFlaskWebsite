708d615c5accd55bf27017a5482dbdb733124dcc
wurfmaul78 <gerhard.posch@outlook.com>
2021-12-26 17:42:01 +0100

    Start Flask, set Enviroment

--- start.py
+++ start.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask (__name__)
 
 @app.route("/")
 def home():
-    return ("<h3>Hello world!<h3>")
+    return "Hello world"
 
 if "__name__"  == "__main__":
     app.run()
