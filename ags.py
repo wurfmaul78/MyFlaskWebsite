8ce9f15e231938c3a09f3c8b81f51170ec1847b9
wurfmaul78 <gerhard.posch@outlook.com>
2021-12-30 17:54:55 +0100

    Return auf String geändert. Fehlermeldung eingebau

--- ags.py
+++ ags.py
@@ -18,19 +18,25 @@ def byplace(place):
     AGSLoad.close()
     for data in AGSJSON['daten']:
         if place in data[1]:
+            data = json.dumps(data)
             return(data)
+    else:
+        return(f'Your Request {place} does not exist or is wrong written. Please try again :-)')
 
-def bykey(place):
+def bykey(key):
     AGSLoad = requests.get(AGSSource, timeout=5)
     AGSLoad.encoding='utf-8'
     AGSJSON = AGSLoad.json()
     AGSLoad.close()
     for data in AGSJSON['daten']:
-        if place in data[0]:
+        if key in data[0]:
+            data = json.dumps(data)
             return(data)
+    else:
+        return(f'Your Request {key} does not exists or is not numeric. Please try again :-)')
 
 if __name__ == ('__main__'):
-    print(byplace('Linz am Rhein'))
+    #print(byplace('Linz am Rhein'))
     print(bykey('07138041'))
    
 
