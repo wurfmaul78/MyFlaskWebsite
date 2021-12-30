f1f1b304d4eabf4c0d926bb94477f4f714d04da8
Gerhard Mac <gerhard.posch@outlook.com>
2021-12-30 21:02:51 +0100

    Test File

--- test_ags.py
+++ test_ags.py
@@ -1,5 +1,13 @@
-from ags import bykey,byplace
-import pytest
+from ags import AGSSource, bykey,byplace, herausgeber
 
-def test_bykey():
-    assert bykey('07138041') ==  ["07138041", "Linz am Rhein, Stadt"]
+def test_herausgeber():
+    assert herausgeber()== 'Statistisches Bundesamt, Wiesbaden'
+
+def test_source():
+    assert AGSSource == "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:ags_2011-04-01/download/AGS_2011-04-01.json"
+ 
+def test_place():
+    assert byplace('Linz am Rhein') == (["07138041", "Linz am Rhein, Stadt"]) 
+
+if (__name__) == ('__main__'):
+    print (byplace('Linz am Rhein'))
