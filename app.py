ce5aaca1c25d31b807da540503fcfdc275703e83
wurfmaul78 <gerhard.posch@outlook.com>
2021-12-30 17:55:18 +0100

    Route place eingebaut

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, render_template
 from markupsafe import escape
+from ags import bykey,byplace
 
 app = Flask (__name__)
 
@@ -7,11 +8,17 @@ app = Flask (__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/place/<place>")
+def agsplace(place):
+    try:
+        return(byplace(place))
+    except:
+        return f"Not possible! Your place {place} not exists or is wrong written. Please try again :-)"
+
 @app.route("/ags/<ags>")
 def agskey(ags):
     try:
-        ags = (int(ags))
-        return f"Ja, ein Integer {escape(ags)}"
+        return(bykey(ags))
     except:
         return f"Not possible! <b>{escape(ags)}</b> is not type integer."
 
