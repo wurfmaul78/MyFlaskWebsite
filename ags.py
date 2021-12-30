16b65a7b78b317da09535ec0db7293d79971c908
Gerhard Mac <gerhard.posch@outlook.com>
2021-12-30 20:13:31 +0100

    Herausgeber als Funktion eingeführt

--- ags.py
+++ ags.py
@@ -2,13 +2,17 @@
 import requests, json
 
 AGSSource  = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:ags_2011-04-01/download/AGS_2011-04-01.json"
-AGSLoad = requests.get(AGSSource)
-AGSJSON = AGSLoad.json()
-AGSLoad.close()
 
-for data in AGSJSON['metadaten']:
-    if ('herausgebernameLang') in data:
-        Herausgeber = (AGSJSON['metadaten']['herausgebernameLang'])
+
+
+def herausgeber():
+    AGSLoad = requests.get(AGSSource, timeout=5)
+    AGSJSON = AGSLoad.json()
+    AGSLoad.close()
+    for data in AGSJSON['metadaten']:
+        if ('herausgebernameLang') in data:
+            Herausgeber = (AGSJSON['metadaten']['herausgebernameLang'])
+            return (Herausgeber)
 
 
 def byplace(place):
