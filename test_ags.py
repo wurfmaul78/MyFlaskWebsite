aaa77ff34e9b7a4c67c698c00e6e1773b4b1b693
wurfmaul78 <gerhard.posch@outlook.com>
2022-06-20 21:15:28 +0200

    Aktuelle Datensource ausgewält

--- test_ags.py
+++ test_ags.py
@@ -2,7 +2,7 @@ import ags
 import pytest
 
 def test_source():
-    assert ags.AGSSource == "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:ags_2011-04-01/download/AGS_2011-04-01.json"
+    assert ags.AGSSource == "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:ags_2022-05-31/download/AGS_2022-05-31.json"
  
 def test_place():
     assert ags.byplace('München') == {'Data': ['09162000', 'München, Landeshauptstadt'], 'Source': 'Statistisches Bundesamt Wiesbaden', 'info': 'gerhard.posch@outlook.com'}
